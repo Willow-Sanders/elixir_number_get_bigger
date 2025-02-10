c492b629f78ca7978a26878973197e0aab18929e
Willow Sanders <willowsanders1000@gmail.com>
2025-02-10 14:10:51 -0600

    add auto_clicker_mult

--- number_get_bigger/lib/number_get_bigger_web/live/idle_live.ex
+++ number_get_bigger/lib/number_get_bigger_web/live/idle_live.ex
@@ -1,14 +1,23 @@
 defmodule NumberGetBiggerWeb.IdleLive do
   use NumberGetBiggerWeb, :live_view
 
-  defp auto_clicker_cost(), do: 100 # points
-  defp tick_frequency(), do: 1000 # milliseconds
+  # points
+  defp auto_clicker_cost(), do: 100
+  # milliseconds
+  defp tick_frequency(), do: 1000
 
   def mount(_params, _session, socket) do
-	  Process.send_after(self(), :tick, tick_frequency())
+    Process.send_after(self(), :tick, tick_frequency())
+
     socket =
       socket
-      |> assign(%{points: 0, auto_clickers: 0, auto_clicker_cost: 100})
+      |> assign(%{
+        points: 0,
+        auto_clickers: 0,
+        auto_clicker_cost: 100,
+        auto_clicker_mult: 1,
+        auto_clicker_mult_cost: 10000
+      })
 
     {:ok, socket}
   end
@@ -17,18 +26,53 @@ defmodule NumberGetBiggerWeb.IdleLive do
     # TODO: refactor to separate document
     ~H"""
     <div>
-      <p>Points: {@points}</p>
-	    <p>Auto Clickers: {@auto_clickers}</p>
+      <%= if @points > 0 do %>
+        <p>Points: {@points}</p>
+	    <% end %>
+    <%= if @auto_clickers > 0 do %>
+      <p>Auto Clickers: {@auto_clickers}</p>
+    <% end %>
+    <%= if @auto_clicker_mult > 1 do %>
+	    <p>Auto Clicker Mult: {@auto_clickers}</p>
+    <% end %>
     </div>
     <br />
     <div>
-      <.button id="manual_click" phx-click="manual_click">Make Number Bigger</.button>
-    </div>
-    <br />
-    <div>
-      <.button id="buy_auto_clicker" phx-click="buy_auto_clicker">
-        Click for me ({@auto_clicker_cost} points)
+      <.button class="bg-emerald-900 hover:bg-emerald-700" id="manual_click" phx-click="manual_click">
+        Make Number Bigger
       </.button>
+    </div>
+    <br />
+    <div>
+      <%= if @points >= @auto_clicker_cost do %>
+        <.button
+          class="bg-emerald-900 hover:bg-emerald-700"
+          id="buy_auto_clicker"
+          phx-click="buy_auto_clicker"
+        >
+          Do it for me ({@auto_clicker_cost} points)
+        </.button>
+      <% else %>
+        <.button id="buy_auto_clicker" phx-click="buy_auto_clicker" disabled>
+          Do it for me ({@auto_clicker_cost} points)
+        </.button>
+      <% end %>
+    </div>
+    <br />
+    <div>
+      <%= if @points >= @auto_clicker_mult_cost do %>
+        <.button
+          class="bg-emerald-900 hover:bg-emerald-700"
+          id="upgrade_auto_clickers"
+          phx-click="upgrade_auto_clickers"
+        >
+          Upgrade Auto Clickers ({@auto_clicker_mult_cost} points)
+        </.button>
+      <% else %>
+        <.button id="upgrade_auto_clickers" phx-click="upgrade_auto_clickers" disabled>
+          Upgrade Auto Clickers ({@auto_clicker_mult_cost} points)
+        </.button>
+      <% end %>
     </div>
     """
   end
@@ -57,12 +101,33 @@ defmodule NumberGetBiggerWeb.IdleLive do
     end
   end
 
-  def handle_info(:tick, socket) do
-	  Process.send_after(self(), :tick, tick_frequency())
-	  socket =
-		  socket
-		  |> assign(:points, socket.assigns.points + socket.assigns.auto_clickers)
+  def handle_event("upgrade_auto_clickers", _params, socket) do
+    if socket.assigns.points >= socket.assigns.auto_clicker_mult_cost do
+      socket =
+        socket
+        |> assign(%{
+          points: socket.assigns.points - socket.assigns.auto_clicker_mult_cost,
+          auto_clicker_mult: round(socket.assigns.auto_clicker_mult * 1.05),
+          auto_clicker_mult_cost: round(socket.assigns.auto_clicker_cost * 1.10)
+        })
 
-	  {:noreply, socket}
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, tick_frequency())
+
+    socket =
+      socket
+      |> assign(
+        :points,
+        socket.assigns.points +
+          round(socket.assigns.auto_clickers * socket.assigns.auto_clicker_mult)
+      )
+
+    {:noreply, socket}
   end
 end
